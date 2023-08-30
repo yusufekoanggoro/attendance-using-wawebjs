3aed7f315c7b45559c47fea39a62460c6079e701
Yusuf Eko Anggoro <anggoroekoyusuf@gmail.com>
2023-08-30 16:31:37 +0700

    add confif

--- config.js
+++ config.js
@@ -4,7 +4,16 @@ const confidence = require('confidence');
 const config = {
     initialAttendance: process.env.INITIAL_ATTANDANCE,
     minSleepmsHandleBlasting: parseInt(process.env.MIN_SLEEPMS_HANDLE_BLASTING),
-    maxSleepmsHandleBlasting: parseInt(process.env.MAX_SLEEPMS_HANDLE_BLASTING)
+    maxSleepmsHandleBlasting: parseInt(process.env.MAX_SLEEPMS_HANDLE_BLASTING),
+    redis: {
+        connection: {
+          host: process.env.REDIS_CLIENT_HOST,
+          port: process.env.REDIS_CLIENT_PORT,
+          passsword: process.env.REDIS_CLIENT_PASSWORD,
+          auth_pass: process.env.REDIS_CLIENT_PASSWORD,
+        },
+        index: process.env.REDIS_CLIENT_INDEX
+    },
 };
 
 const store = new confidence.Store(config);
