a38073b4d65503921640736532b2c59691f3b43d
Yusuf Eko Anggoro <anggoroekoyusuf@gmail.com>
2023-08-31 21:28:48 +0700

    refactor code

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,16 +1,17 @@
-const { createLogger, format, transports } = require('winston');
-
-const { combine, printf } = format;
+const { createLogger, format, transports,  } = require('winston');
 
 // Custom log format
-const customFormat = printf(({ timestamp, level, message }) => `${timestamp} [${level}]: ${message}`);
+const customFormat = format.combine(
+  format.colorize(),
+  format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+  format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} [${level}]: ${message}`;
+  })
+);
 
 // Create a new logger instance
 const logger = createLogger({
-  format: combine(
-    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-    customFormat,
-  ),
+  format: customFormat,
   transports: [
     new transports.Console(), // Log to console
     new transports.File({ filename: 'app.log' }), // Log to file
