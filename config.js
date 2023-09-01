9a22e81962caa3787a66ceb381879a9966ace651
Yusuf Eko Anggoro <anggoroekoyusuf@gmail.com>
2023-09-01 07:03:50 +0700

    refactor code

--- config.js
+++ config.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const confidence = require('confidence');
 
 const config = {
-  initialAttendance: process.env.INITIAL_ATTANDANCE,
   minSleepmsHandleBlasting: parseInt(process.env.MIN_SLEEPMS_HANDLE_BLASTING),
   maxSleepmsHandleBlasting: parseInt(process.env.MAX_SLEEPMS_HANDLE_BLASTING),
   redis: {
