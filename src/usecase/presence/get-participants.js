dc4d20628c21c471162360b182056114b211abdb
Yusuf Eko Anggoro <anggoroekoyusuf@gmail.com>
2023-09-01 07:49:40 +0700

    refactor code

--- src/usecase/presence/get-participants.js
+++ src/usecase/presence/get-participants.js
@@ -4,28 +4,36 @@ const sharedUc = require('./shared');
 
 const getParticipants = async (payload) => {
   try {
-    const filePathUserMaster = await sharedUc.getFilePathUserMaster(payload.groupInfo);
+    const filePathUserMaster = await sharedUc.getFilePathUserMaster();
     if(filePathUserMaster.err) return wrapper.error('file not found');
 
     const csvHandler = new CSVHandler(filePathUserMaster.data);
     const userData = await csvHandler.readAllRecords();
-    
-    let participant = [];
-    if (!userData.err) participant = userData.data;
 
-    const newAttendanceData = [];
+    let allRegisteredUser = [];
+    if (!userData.err) allRegisteredUser = userData.data;
+
+    let filteredUsers = [];
+    for (const participant of payload.chat.participants) {
+      const foundUser = allRegisteredUser.filter((v) => v.wa_number === participant.id._serialized);
+      if (foundUser.length) {
+        filteredUsers.push(foundUser[0])
+      }
+    }
+
+    const registeredUserInGrup = [];
     let sequenceNumber = 1;
-    participant.forEach((data) => {
-      newAttendanceData.push(`${sequenceNumber}. ${data.npm} ${data.full_name}\n`);
+    filteredUsers.forEach((data) => {
+      registeredUserInGrup.push(`${sequenceNumber}. ${data.npm} ${data.full_name}\n`);
       sequenceNumber += 1;
     });
 
     let finalString = `Daftar Peserta\n\n`;
-    newAttendanceData.forEach((v) => {
+    registeredUserInGrup.forEach((v) => {
       finalString += v;
     });
 
-    finalString += `\nTotal: ${newAttendanceData.length} Mahasiswa/i`;
+    finalString += `\nTotal: ${registeredUserInGrup.length} Mahasiswa/i`;
     return wrapper.data(finalString);
   } catch (error) {
     return wrapper.error(error);
